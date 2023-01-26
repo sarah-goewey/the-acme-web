54713596c1261d5d9d2a7e9e1e4630c1d93efcb9
Sarah Goewey <sarah.goewey@yahoo.com>
2023-01-25 20:28:59 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+const server = http.createServer((req, res)=>{
+    res.write('hello world')
+    res.end()
+})
+
+const port = process.env.PORT || 3000
+server.listen(port, ()=>{
+    console.log(`listening on port ${port}`)
+})
